1e08575741313870cc02f7a70bc2fb2a35a53a15
Moxvallix Mox <moxvallix@gmail.com>
2023-02-14 01:13:10 +1030

    feat(design): implement basic header design, add icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        minecraft: ['Minecraft', ...defaultTheme.fontFamily.mono],
+      },
+      colors: {
+        'header-start': '#af4890',
+        'header-end': '#4b51b5',
       },
     },
   },
